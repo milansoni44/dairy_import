79277110b26d8b02103d26fa007749f6c3c590ce
Milan Soni <milansoni44@gmail.com>
2016-11-09 14:44:37 +0530

    Structured code

--- core/Db.php
+++ core/Db.php
@@ -1,7 +1,7 @@
 <?php
 
 class Db {
-
+    // define all static variables
     public static $localhost = "";
     public static $user = "";
     public static $pass = "";
@@ -9,12 +9,18 @@ class Db {
     public static $conn = "";
     
     public function __construct() {
+        //initialize with data
         self::$localhost = "localhost";
         self::$user = "root";
         self::$pass = "";
         self::$db = "";
         self::$conn = "";
     }
+    /**
+     * connection to database
+     * @param type $db
+     * @return type
+     */
     public function connect($db = NULL) {
         self::$db = $db;
         self::$conn = mysqli_connect(self::$localhost, self::$user, self::$pass, self::$db);
@@ -24,18 +30,31 @@ class Db {
         }
         return self::$conn;
     }
-
+    /**
+     * select query
+     * @param type $table
+     * @param type $params1
+     * @param type $params2
+     * @return type
+     */
     public static function select($table = NULL, $params1 = NULL, $params2 = NULL) {
         $sql = "SELECT * FROM $table WHERE config_category = '$params1' AND config_name = '$params2'";
         $result = mysqli_query(self::$conn, $sql);
         return $result;
     }
-
+    /**
+     * truncate data from the table
+     * @param type $table
+     */
     public static function truncate($table = NULL) {
         $sql = "TRUNCATE TABLE $table";
         $result = mysqli_query(self::$conn, $sql);
     }
-
+    /**
+     * insert data to table
+     * @param type $data
+     * @param type $table
+     */
     public static function insert($data = array(), $table = NULL) {
         if (is_array($data) && !empty($data)) {
             foreach ($data as $k => $v) {
--- models/Models.php
+++ models/Models.php
@@ -17,19 +17,35 @@ class Models extends Db{
     public function __construct() {
         parent::__construct();
     }
-    
+    /**
+     * Connection to database
+     * @param type $db
+     */
     public function connect($db = NULL) {
         parent::connect($db);
     }
-    
+    /**
+     * select query with parameters
+     * @param type $table
+     * @param type $params1
+     * @param type $params2
+     * @return type
+     */
     public static function select($table = NULL, $params1 = NULL, $params2 = NULL) {
         return DB::select($table, $params1, $params2);
     }
-    
+    /**
+     * truncate data from the table
+     * @param type $table
+     */
     public static function truncate($table = NULL) {
         Db::truncate($table);
     }
-    
+    /**
+     * insert data into table
+     * @param type $data
+     * @param type $table
+     */
     public static function insert($data = array(), $table = NULL) {
         Db::insert($data, $table);
     }
